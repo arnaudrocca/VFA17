d79f38219ceebd3049f59fc9ee9f63ead0167c80
Yann Kubacki <kubackiyann@gmail.com>
2016-05-03 14:31:59 +0200

    UI developpement

--- src/scripts/components/app.js
+++ src/scripts/components/app.js
@@ -17,16 +17,26 @@ class App extends React.Component {
 				</div>
 				<div className="wireframe-side wireframe-side--right">
 					<span className="wireframe-side__trapezoid"></span>
-					<span className="wireframe-side__arrow"></span>
+					<svg className="wireframe-side__arrow" width="15" height="15">
+						<path d="M12.1,14.1L0.8,2.8c-0.8-0.8-0.2-2.3,0.9-2.3H13c0.7,0,1.3,0.6,1.3,1.3v11.3C14.4,14.3,12.9,14.9,12.1,14.1z"/>
+					</svg>
 				</div>
 				<div className="wireframe-side wireframe-side--bottom">
 					<span className="wireframe-side__trapezoid"></span>
 				</div>
 				<div className="wireframe-side wireframe-side--left">
 					<span className="wireframe-side__trapezoid"></span>
-					<span className="wireframe-side__arrow"></span>
+					<svg className="wireframe-side__arrow" width="15" height="15">
+						<path d="M12.1,14.1L0.8,2.8c-0.8-0.8-0.2-2.3,0.9-2.3H13c0.7,0,1.3,0.6,1.3,1.3v11.3C14.4,14.3,12.9,14.9,12.1,14.1z"/>
+					</svg>
 				</div>
 				{this.props.children}
+
+				<div className="toolbar">
+					<button className="toolbar__btn"><span>Infos</span></button>
+					<button className="toolbar__btn"><span>picto</span></button>
+					<button className="toolbar__btn"><span>picto</span></button>
+				</div>
 			</div>
 		)
 
