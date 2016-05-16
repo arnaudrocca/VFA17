b496c847b911b50b79fb42d03e1de0f792f3a4fc
Arnaud ROCCA <arnaud.rocca@gmail.com>
2016-05-16 16:08:32 +0200

    Fix hotpoints

--- src/scripts/components/Hotpoint.js
+++ src/scripts/components/Hotpoint.js
@@ -7,14 +7,25 @@ class Hotpoint extends React.Component {
 
 		super()
 
+		this.radius = 5
+
 	}
 
 	getContent() {
 
+		this.data = hotpointsData.find((hotpointData) => {
+			return hotpointData.id == this.props.hotpoint.id
+		})
+
 		if (this.props.hotpoint.answers.length > 0) {
-			this.hotpoint = <circle className="hotpoint" cx={this.props.x} cy={this.props.y} r="5" fill="#AAA" />
+			this.hotpoint = <span></span>
+			this.position = {
+				left: this.data.x,
+				top: this.data.y
+			}
 		} else {
 			this.hotpoint = ''
+			this.position = {}
 		}
 
 	}
@@ -23,12 +34,8 @@ class Hotpoint extends React.Component {
 
 		let dialog = ''
 
-		for (let answer of this.props.hotpoint.answers) {
-			const hotpointDatum = hotpointsData.find((hotpointData) => {
-				return hotpointData.mapId == this.props.hotpoint.id && hotpointData.answer == answer
-			})
-
-			dialog += hotpointDatum.text
+		for (let i in this.props.hotpoint.answers) {
+			dialog += this.data.text
 		}
 
 		this.props.onClick(dialog)
@@ -40,9 +47,9 @@ class Hotpoint extends React.Component {
 		this.getContent()
 
 		return (
-			<g onClick={this.clickHandler.bind(this)}>
+			<div className="hotpoint" onClick={this.clickHandler.bind(this)} style={this.position}>
 				{this.hotpoint}
-            </g>
+            </div>
 		)
 
 	}
--- src/scripts/components/Hotpoints.js
+++ src/scripts/components/Hotpoints.js
@@ -11,29 +11,9 @@ class Hotpoints extends React.Component {
 
     getContent() {
 
-        const hotpointsPosition = [
-            {
-                x: 600,
-                y: 250
-            },
-            {
-                x: 400,
-                y: 400
-            },
-            {
-                x: 300,
-                y: 300
-            },
-            {
-                x: 500,
-                y: 500
-            }
-        ]
-
         this.hotpoints = this.props.hotpoints.map((hotpoint, index) => {
             return (
-                <Hotpoint key={index} onClick={this.props.onClick} hotpoint={this.props.hotpoints[index]}
-                    x={hotpointsPosition[index].x} y={hotpointsPosition[index].y} />
+                <Hotpoint key={index} onClick={this.props.onClick} hotpoint={this.props.hotpoints[index]} />
             )
         })
 
@@ -44,9 +24,9 @@ class Hotpoints extends React.Component {
         this.getContent()
 
 		return (
-			<g>
+			<div className="hotpoints">
                 {this.hotpoints}
-            </g>
+            </div>
 		)
 
 	}
