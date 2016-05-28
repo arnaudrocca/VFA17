f7aae67081fae3b2f2a1bf278c1976c2b09f8562
Yann Kubacki <kubackiyann@gmail.com>
2016-05-28 18:13:01 +0200

    Menu hover animations setup

--- src/scripts/components/Menu.js
+++ src/scripts/components/Menu.js
@@ -42,6 +42,7 @@ class Menu extends React.Component {
 
 		const menuBtn = ReactDOM.findDOMNode(this.refs.menuBtn)
 		const menuDragLine = ReactDOM.findDOMNode(this.refs.menuDragLine)
+		const slices = document.querySelectorAll('.menu__slice')
 
 		const windowWidth = window.innerWidth
 		const sideSize = 20
@@ -65,11 +66,62 @@ class Menu extends React.Component {
 				document.body.classList.add('is-menu-active')
 				menuBtn.classList.add('is-active')
 			},
-			onDrag: function() {
-				TweenMax.set(menuDragLine, {width: Math.abs(this.x)})
+			onDrag: function(endValue) {
+				TweenMax.to(menuDragLine, 0,{width: Math.abs(this.x)})
+				console.log(endValue)
+
+				let currentSlice = ''
+
+				switch(Math.round(endValue.x / columnWidth) * columnWidth) {
+					case (columnWidth):
+						console.log('hello coco 1', Math.round(endValue.x / columnWidth) * columnWidth)
+						currentSlice = document.querySelector('.menu__slice--0')
+					break;
+					case (columnWidth * 2):
+						console.log('hello coco 2', Math.round(endValue.x / columnWidth) * columnWidth)
+						currentSlice = document.querySelector('.menu__slice--1')
+					break;
+					case (columnWidth * 3):
+						console.log('hello coco 3', Math.round(endValue.x / columnWidth) * columnWidth)
+						currentSlice = document.querySelector('.menu__slice--2')
+					break;
+					case (columnWidth * 4):
+						console.log('hello coco 4', Math.round(endValue.x / columnWidth) * columnWidth)
+						currentSlice = document.querySelector('.menu__slice--3')
+					break;
+					case (columnWidth * 5):
+						console.log('hello coco 5', Math.round(endValue.x / columnWidth) * columnWidth)
+						currentSlice = document.querySelector('.menu__slice--4')
+						console.log(currentSlice)
+					break;
+				}
+
+				if(currentSlice != ''){
+					for (var i = slices.length - 1; i >= 0; i--) {
+						slices[i].classList.remove('is-active')
+					}
+					currentSlice.classList.add('is-active')
+					// TweenMax.to('.menu__slice', .3, {
+					// 	backgroundColor: 'transparent'
+					// })
+					// TweenMax.to(currentSlice, .3, {
+					// 	// backgroundColor: 'rgba(41,48,87,.15)'
+					// 	backgroundColor: 'rgba(255,255,255,.1)'
+					// })
+				} else {
+					for (var i = slices.length - 1; i >= 0; i--) {
+						slices[i].classList.remove('is-active')
+					}
+					// TweenMax.to('.menu__slice', .3, {
+					// 	backgroundColor: 'transparent'
+					// })
+				}
 			},
 			onRelease: (endValue) => {
 				menuBtn.classList.remove('is-active')
+				for (var i = slices.length - 1; i >= 0; i--) {
+					slices[i].classList.remove('is-active')
+				}
 
 				const selectedId = Math.floor(endValue.x / columnWidth)
 
@@ -140,9 +192,9 @@ class Menu extends React.Component {
 		this.menuItems = this.props.menuState.map((item, index) => {
 			if (item.state == 'todo') {
 				return (
-					<div key={index} className="menu__slice">
+					<div key={index} className={`menu__slice menu__slice--todo menu__slice--${index}`}>
 						<div className="menu__item">
-							<IconTodo width="75" opacity="0.6" color="#ffffff"/>
+							<IconTodo classes="icon-todo" width="75" opacity="0.6" color="#ffffff"/>
 							<span className="menu__label">Voyage {index + 1}</span>
 						</div>
 					</div>
@@ -150,7 +202,7 @@ class Menu extends React.Component {
 			}
 			else if (item.state == 'locked') {
 				return (
-					<div key={index} className="menu__slice">
+					<div key={index} className={`menu__slice menu__slice--${index}`}>
 						<div className="menu__item">
 							<IconLocked width="75" opacity="0.6" color="#ffffff"/>
 							<span className="menu__label">Voyage {index + 1}</span>
@@ -160,7 +212,7 @@ class Menu extends React.Component {
 			}
 			else {
 				return (
-					<div key={index} className="menu__slice">
+					<div key={index} className={`menu__slice menu__slice--${index}`}>
 						<div className="menu__item">
 							<IconDone width="75" opacity="0.6" color="#ffffff"/>
 							<span className="menu__label">Voyage {index + 1}</span>
