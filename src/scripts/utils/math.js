27c1e4d8855dbb42b08ad9caa5a58ee247588f80
Arnaud ROCCA <arnaud.rocca@gmail.com>
2016-06-16 19:25:06 +0200

    Update math

--- src/scripts/utils/math.js
+++ src/scripts/utils/math.js
@@ -1,12 +1,13 @@
 /**
- * Re-maps a number from one range to another
- *
+ * @function
+ * @name map
+ * @description Re-maps a number from one range to another
  * @param  {number} value   - The incoming value to be converted
  * @param  {number} start1  - Lower bound of the value's current range
  * @param  {number} stop1   - Upper bound of the value's current range
  * @param  {number} start2  - Lower bound of the value's target range
  * @param  {number} stop2   - Upper bound of the value's target range
- * @return {number}         Remapped number
+ * @return {number}         - Remapped number
  */
 export function map(value, start1, stop1, start2, stop2) {
 
